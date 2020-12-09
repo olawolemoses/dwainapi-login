d19c64632ee3145af27017b03849e200ede2f29a
olawolemoses <olawolemoses@users.noreply.github.com>
2020-12-09 15:58:15 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,10 +11,10 @@ class ExampleTest extends TestCase
     }
 
     /** @test */
-    public function it_can_display_the_route_from_auth() {
+    public function it_can_display_the_route_from_auth()
+    {
         $this
             ->get('authentication')
             ->assertOk();
-
     }
 }
